ca79eef67bb3af679bd9f52f63b6dca7568e34a7
Nguyen Trien <trienvaj@gmail.com>
2020-12-15 16:30:09 +0700

    finished chap1,2

---
+++ javascript/chapter1/quiz/main.js
@@ -0,0 +1,5 @@
+alert('welcom to Quiz Ninja!')
+
+const question = "What is Supperman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
---
+++ javascript/chapter2/main.js
@@ -0,0 +1,9 @@
+var i = typeof 'hello'
+console.log(i)
+typeof 10
+
+typeof true;
+
+typeof { ninja: 'turtle' }
+
+typeof [1, 2, 3]
